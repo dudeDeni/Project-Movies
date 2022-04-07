820aa1809899b9462d943fc1e5868e2e4e863b92
dudeDeni <daniillatt@gmail.com>
2022-04-07 13:28:49 +0300

    test [Delivers #181819421

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const port = 8080;
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./docs/swagger.json");
 
+const movies = [
+	{ id: 1, name: "Indiana Jones", price: 19.59 },
+	{ id: 2, name: "Matrix", price: 19.59 },
+	{ id: 3, name: "Star Wars", price: 19.59 },
+	{ id: 4, name: "Sin City", price: 19.59 },
+	{ id: 5, name: "Who killed Rogger Rabbit", price: 19.59 },
+	{ id: 6, name: "Kill Bill", price: 19.59 },
+	{ id: 7, name: "Pulp Fiction", price: 19.59 },
+	{ id: 8, name: "Saving Private Banner", price: 19.59 },
+];
+
 app.get("/movies", (req, res) => {
-	res.send(["Indiana Jones", "Matrix"]);
+	res.send(movies);
+});
+
+app.get("/movies/:id", (req, res) => {
+	res.send(movies[req.params.id]);
 });
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
