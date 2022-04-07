68f066cb36ba688f321aaeb94e26794064c595d7
dudeDeni <daniillatt@gmail.com>
2022-04-07 16:16:48 +0300

    Created Post and Delete, requires some fixes in Apicurio api

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const port = 8080;
 const swaggerUi = require("swagger-ui-express");
 const yamljs = require("yamljs");
 const swaggerDocument = yamljs.load("./docs/swagger.yaml");
+
+app.use(express.json());
 
 const movies = [
 	{ id: 1, name: "Indiana Jones", price: 19.59 },
@@ -27,8 +30,41 @@ app.get("/movies/:id", (req, res) => {
 	res.send(movies[req.params.id - 1]);
 });
 
+app.post("/movies", (req, res) => {
+	if (!req.body.name || !req.body.price) {
+		return res.status(400).send({ error: "One or all params are missing" });
+	}
+	let movie = {
+		id: movies.length + 1,
+		price: req.body.price,
+		name: req.body.name,
+	};
+	movies.push(movie);
+
+	res
+		.status(201)
+		.location(`${getBaseUrl(req)}/movies/${movies.length}}`)
+		.send(movie);
+});
+
+app.delete("/movies/:id", (req, res) => {
+	if (typeof movies[req.params.id - 1] === "undefined") {
+		return res.status(404).send({ error: "Movie not found" });
+	}
+
+	movies.splice(req.params.id - 1, 1);
+
+	res.status(204).send({ error: "No content" });
+});
+
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
 	console.log(`API up at: http://localhost:${port}`);
 });
+
+function getBaseUrl(req) {
+	return req.connection && req.connection.encrypted
+		? "https"
+		: "http" + `://${req.headers.host}`;
+}
